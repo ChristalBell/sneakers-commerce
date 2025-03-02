fe88ce0a9917438e8625e9080298db369ce47ff9
Christal Bell <christalvictoriabell@gmail.com>
2025-03-02 00:13:46 -0800

    carousel main functions

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -15,8 +15,8 @@ const Carousel = () => {
   };
 
   const nextSlide = () => {
-    const nextIndex = index - 1;
-    setIndex(nextIndex < 0 ? length++ : nextIndex);
+    const nextIndex = index + 1;
+    setIndex(nextIndex < 0 ? length + 1 : nextIndex);
   };
 
   const shoePictures = [
@@ -49,84 +49,25 @@ const Carousel = () => {
           width={390}
           height={400}
         />
-
-        <Image
-          className="mainSlide"
-          style={{
-            borderRadius: ".75rem",
-            position: "absolute",
-            inset: 0,
-            opacity: 0,
-          }}
-          src="/image-product-2.jpg"
-          alt="main"
-          width={390}
-          height={400}
-        />
-        <Image
-          className="mainSlide"
-          style={{
-            borderRadius: ".75rem",
-            position: "absolute",
-            inset: 0,
-            opacity: 0,
-          }}
-          src="/image-product-3.jpg"
-          alt="main"
-          width={390}
-          height={400}
-        />
-        <Image
-          className="mainSlide"
-          style={{
-            borderRadius: ".75rem",
-            position: "absolute",
-            inset: 0,
-            opacity: 0,
-          }}
-          src="/image-product-4.jpg"
-          alt="main"
-          width={390}
-          height={400}
-        />
       </Box>
 
       <Box className="thumbnails" sx={{ marginTop: "2rem" }}>
-        <Image
-          style={{
-            borderRadius: ".75rem",
-            marginRight: "1.5rem",
-          }}
-          src="/image-product-1-thumbnail.jpg "
-          alt="shoe1"
-          width={80}
-          height={100}
-          className="slideOption"
-        />
-        <Image
-          style={{ borderRadius: ".75rem", marginRight: "1.5rem" }}
-          src="/image-product-2-thumbnail.jpg "
-          alt="shoe2"
-          width={80}
-          height={100}
-          className="slideOption"
-        />
-        <Image
-          style={{ borderRadius: ".75rem", marginRight: "1.5rem" }}
-          src="/image-product-3-thumbnail.jpg"
-          alt="shoe3"
-          width={80}
-          height={100}
-          className="slideOption"
-        />
-        <Image
-          style={{ borderRadius: ".75rem" }}
-          src="/image-product-4-thumbnail.jpg "
-          alt="shoe4"
-          width={80}
-          height={100}
-          className="slideOption"
-        />
+        {shoePictures.map((shoePicture) => {
+          return (
+            <Image
+              key={shoePicture}
+              style={{
+                borderRadius: ".75rem",
+                marginRight: "1.5rem",
+              }}
+              src={shoePicture}
+              alt="shoe1"
+              width={80}
+              height={100}
+              className="slideOption"
+            />
+          );
+        })}
       </Box>
 
       <Box
@@ -137,22 +78,10 @@ const Carousel = () => {
           marginTop: ".75rem",
         }}
       >
-        <Button
-          value="prev"
-          sx={{ color: COLORS.orange }}
-          className="choose"
-          id="prev"
-          onClick={previousSlide}
-        >
+        <Button sx={{ color: COLORS.orange }} id="prev" onClick={previousSlide}>
           PREV
         </Button>
-        <Button
-          value="next"
-          sx={{ color: COLORS.orange }}
-          className="choose"
-          id="next"
-          onClick={nextSlide}
-        >
+        <Button sx={{ color: COLORS.orange }} id="next" onClick={nextSlide}>
           NEXT
         </Button>
       </Box>
